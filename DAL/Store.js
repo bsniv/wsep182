c89e2826eaf32a5cbb66b9cafb180bc57234a64c
itamarBitton <itamarbitton13@gmail.com>
2018-04-01 18:49:43 +0300

    User,StoreOwners, Store functions in DAL added

--- DAL/Store.js
+++ DAL/Store.js
@@ -2,9 +2,40 @@ var sqlite3 = require('sqlite3').verbose();
 
 
 module.exports = {
-    get: function () {
+    get: function (item) {
         return new Promise((resolve,reject)=>{
-            resolve(0);
+            var db = new sqlite3.Database('DataBase.db');
+        var all=0;
+        var queryString = "select * from Store where";
+        if(item.storeId!=null){
+            queryString+=" storeId is '"+item.storeId+"'";
+            all++;
+        }
+        if(item.storeName!=null){
+            if(all!=0){
+                queryString+=" and";
+            }
+            queryString+=" storeName = "+item.storeName;
+            all++;
+        }
+        if(item.isActive!=null){
+            if(all!=0){
+                queryString+=" and";
+            }
+            queryString+=" isActive = "+item.isActive;
+            all++;
+        }
+        if(all==0){
+            queryString = "select * from Store";
+        }
+        db.all(queryString,function (err,rows){
+            if(err){
+                reject(queryString);
+            }
+            else{
+                resolve(rows);
+            }
+        });
     });
     },
     /**
@@ -13,18 +44,17 @@ module.exports = {
     set: function(item){
         return new Promise((resolve,reject)=> {
             var db = new sqlite3.Database('DataBase.db');
-        db.run("insert into Store\n" +
-            "(storeId, storeName, isActive)\n" +
-            "VALUES (" + item.storeId + ",'" + item.storeName + "'," + item.isActive+");",
+        db.all("insert into Store " +
+            "VALUES ('" + item.storeId + "','" + item.storeName + "','" + item.isActive+"');",
             function (err) {
                 if (err){
-                    reject("error");
+                    reject("error1");
                 }
                 else{
-                    db.run("select * from Store where storeId = "+item.storeId+" and " +
-                        "storeName = "+item.storeName+" and isActive = "+item.isActive,function(err,rows){
+                    db.all("select * from Store where storeId = '"+item.storeId+"' and " +
+                        "storeName = '"+item.storeName+"' and isActive = '"+item.isActive+"'",function(err,rows){
                         if(err){
-                            reject("error");
+                            reject("error2");
                         }
                         else{
                             resolve(rows[0]);
@@ -41,16 +71,16 @@ module.exports = {
     update: function(item){
         return new Promise((resolve,reject)=> {
             var db = new sqlite3.Database('DataBase.db');
-        db.run("UPDATE Store \n"
-            +"SET storeName = "+item.storeName + ", isActive = "+item.isActive+" \n"
-            +"WHERE storeId = "+item.storeId,
+        db.all("UPDATE Store \n"
+            +"SET storeName = '"+item.storeName + "', isActive = '"+item.isActive+"' \n"
+            +"WHERE storeId = '"+item.storeId+"'",
             function (err) {
                 if (err){
                     reject("error");
                 }
                 else{
-                    db.run("select * from Store where storeId = "+item.userName+" and " +
-                        "storeName = "+item.storeName+" and isActive = "+item.isActive,function(err,rows){
+                    db.all("select * from Store where storeId = '"+item.storeId+"' and " +
+                        "storeName = '"+item.storeName+"' and isActive = '"+item.isActive+"'",function(err,rows){
                         if(err){
                             reject("error");
                         }
@@ -69,8 +99,8 @@ module.exports = {
     remove: function(item){
         return new Promise((resolve,reject)=> {
             var db = new sqlite3.Database('DataBase.db');
-        db.run("DELETE FROM Store \n"
-            +"WHERE storeId = "+item.storeId,
+        db.all("DELETE FROM Store \n"
+            +"WHERE storeId = '"+item.storeId+"'",
             function (err) {
                 if (err){
                     reject("error");
--- DAL/Users.js
+++ DAL/Users.js
@@ -2,9 +2,47 @@ var sqlite3 = require('sqlite3').verbose();
 
 
 module.exports = {
-    get: function () {
+    get: function (item) {
         return new Promise((resolve,reject)=>{
-            resolve(0);
+            var db = new sqlite3.Database('DataBase.db');
+        var all=0;
+        var queryString = "select * from Users where";
+        if(item.userName!=null){
+            queryString+=" userName is '"+item.userName+"'";
+            all++;
+        }
+        if(item.password!=null){
+            if(all!=0){
+                queryString+=" and";
+            }
+            queryString+=" password = "+item.password;
+            all++;
+        }
+        if(item.isActive!=null){
+            if(all!=0){
+                queryString+=" and";
+            }
+            queryString+=" isActive = "+item.isActive;
+            all++;
+        }
+        if(item.isSuperUser!=null){
+            if(all!=0){
+                queryString+=" and";
+            }
+            queryString+=" isSuperUser = "+item.isSuperUser;
+            all++;
+        }
+        if(all==0){
+            queryString = "select * from Users";
+        }
+        db.all(queryString,function (err,rows){
+            if(err){
+                reject(queryString);
+            }
+            else{
+                resolve(rows);
+            }
+        });
     });
     },
     /**
@@ -13,18 +51,18 @@ module.exports = {
     set: function(item){
         return new Promise((resolve,reject)=> {
             var db = new sqlite3.Database('DataBase.db');
-            db.run("insert into Users\n" +
-                "(userName, password, isActive, isSuperUser)\n" +
-                "VALUES (" + item.userName + ",'" + item.password + "'," + item.isActive + ",'" + item.isSuperUser ");",
+
+            db.all("INSERT into Users " +
+                "values ('"+item.userName+"','"+ item.password +"','" + item.isActive + "','" + item.isSuperUser +"');",
                 function (err) {
                     if (err){
                         reject("error");
                     }
                     else{
-                        db.run("select * from Products where userName = "+item.userName+" and " +
-                            "password = "+item.password+" and isActive = "+item.basePrice+" and isSuperUser = "+item.isSuperUser,function(err,rows){
+                        db.all("select * from Users where userName = '"+item.userName+"' and " +
+                            "password = '"+item.password+"' and isActive = '"+item.isActive+"' and isSuperUser = '"+item.isSuperUser+"'",function(err,rows){
                             if(err){
-                                reject("error");
+                                reject("error2");
                             }
                             else{
                                 resolve(rows[0]);
@@ -41,16 +79,16 @@ module.exports = {
     update: function(item){
         return new Promise((resolve,reject)=> {
             var db = new sqlite3.Database('DataBase.db');
-        db.run("UPDATE Users \n"
-            +"SET password = "+item.password + ", isActive = "+item.isActive+", isSuperUser = "+item.isSuperUser+" \n"
-            +"WHERE username = "+item.username,
+        db.all("UPDATE Users "
+            +"SET password = '"+item.password + "', isActive = '"+item.isActive+"', isSuperUser = '"+item.isSuperUser+"' "
+            +"WHERE userName = '"+item.userName+"'",
             function (err) {
                 if (err){
                     reject("error");
                 }
                 else{
-                    db.run("select * from Products where userName = "+item.userName+" and " +
-                        "password = "+item.password+" and isActive = "+item.basePrice+" and isSuperUser = "+item.isSuperUser,function(err,rows){
+                    db.all("select * from Users where userName = '"+item.userName+"' and " +
+                        "password = '"+item.password+"' and isActive = '"+item.isActive+"' and isSuperUser = '"+item.isSuperUser+"'",function(err,rows){
                         if(err){
                             reject("error");
                         }
@@ -69,8 +107,8 @@ module.exports = {
     remove: function(item){
         return new Promise((resolve,reject)=> {
             var db = new sqlite3.Database('DataBase.db');
-        db.run("DELETE FROM Users \n"
-            +"WHERE username = "+item.username,
+        db.all("DELETE FROM Users \n"
+            +"WHERE userName = '"+item.userName+"'",
             function (err) {
                 if (err){
                     reject("error");
