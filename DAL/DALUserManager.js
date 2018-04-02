01254c408e5e1b225c6386ac6314eb15e8647ee9
shaybenh7 <tp1002@gmail.com>
2018-04-02 10:30:56 +0300

    NEW USER DALMANAGER

---
+++ DAL/DALUserManager.js
@@ -0,0 +1,55 @@
+
+var Product=require('./Product');
+
+
+module.exports = {
+
+    get: function(type,item){
+        if(type === 'User'){
+            return Users.get(item);
+        }
+        return new Promise((resolve,reject)=>{
+            resolve(item);
+    });
+
+    },
+    set: function(type,item){
+        if(type === 'User') {
+            return Product.set(item);
+        }
+        return new Promise((resolve,reject)=>{
+            resolve(item);
+    });
+    },
+    update: function(type,item){
+        if(type === 'User') {
+            return Product.update(item);
+        }
+        return new Promise((resolve,reject)=>{
+            resolve(item);
+    });
+    },
+    remove: function(type,item){
+        if(type === 'User') {
+            return Product.remove(item);
+        }
+        return new Promise((resolve,reject)=>{
+            resolve(item);
+    });
+    },
+    authentication: function(username,password){
+        if(username == 'admin' && password == 'admin')
+            return new Promise((resolve,reject)=>{
+                resolve(true);
+    });
+        return new Promise((resolve,reject)=>{
+            get('User',username)
+    .then((result)=>
+        {
+            resolve(result.username == username && result.password == password);
+    })
+    .catch((err)=>{reject(err);});
+    });}
+
+}
+
